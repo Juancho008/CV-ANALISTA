f07f5417376932646db61783a38360ad5c027914
Juancho <105228069+Juancho008@users.noreply.github.com>
2022-06-07 23:33:13 -0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+const typed =new Typed(".typed", {
+   strings: [
+    "<b>Full Stack Developer</b>",
+    "<b>Analista en Sistemas</b>",
+    "<b>Front-End Developer</b>",
+	"<b>Marketing Digital</b>"
+  ],
+	stringsElement: '#cadenas-texto',
+	typeSpeed: 75,
+	startDelay: 300,
+	backSpeed: 75,
+	smartBackspace: true, 
+	shuffle: false, 
+	loop: true, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html',
+});
