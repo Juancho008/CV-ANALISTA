2b2814816c76e6adc24f646b4c40e9e939296927
Juan Espindola <juanignacioespindola08@gmail.com>
2025-01-04 19:00:17 -0300

    Ajuste al front de mi porfolio cambiando la presentacion y agregando animaciones y eventos de js para resaltar el efecto parallax

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,33 @@
-const typed =new Typed(".typed", {
-   strings: [
-    "<b>Full Stack Developer</b>",
-    "<b>Analista en Sistemas</b>",
-    "<b>Front-End Developer</b>",
-	"<b>Marketing Digital</b>"
-  ],
-	stringsElement: '#cadenas-texto',
-	typeSpeed: 75,
-	startDelay: 300,
-	backSpeed: 75,
-	smartBackspace: true, 
-	shuffle: false, 
-	loop: true, 
-	loopCount: false, 
-	showCursor: true, 
-	cursorChar: '|', 
-	contentType: 'html',
+const typed = new Typed(".typed", {
+    strings: [
+        "<b>Desarrollador WEB</b>",
+        "<b>Desarrollador Front-End</b>",
+        "<b>Analista en Sistemas</b>"
+
+    ],
+    typeSpeed: 50,
+    startDelay: 200,
+    backSpeed: 50,
+    smartBackspace: false,
+    shuffle: false,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+    contentType: 'html',
 });
+const menu = document.querySelector('.menu');
+const typeds = document.querySelector('.typed');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        menu.classList.add('scrolled');
+        typeds.classList.add('display-typed');
+    } else {
+        menu.classList.remove('scrolled');
+        typeds.classList.remove('display-typed');
+
+    }
+});
+
+setTimeout(hideTyped, 5000);
+setTimeout(showTyped, 10000);
