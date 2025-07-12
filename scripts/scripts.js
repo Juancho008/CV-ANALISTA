d805ba74530627b25b6ab5eab1b08f4f5f0d5f13
Juancho08 <juanignacioespindola08@gmail.com>
2025-07-12 19:25:23 -0300

    imagenes y cambios de tamaño en carousell

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,3 +67,35 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    const nextButton = document.querySelector('.carousel-btn.next');
+    const prevButton = document.querySelector('.carousel-btn.prev');
+
+    let currentIndex = 0;
+
+    function updateSlidePosition() {
+        const slideWidth = slides[0].getBoundingClientRect().width;
+        track.style.transform = 'translateX(' + (-slideWidth * currentIndex) + 'px)';
+    }
+
+    nextButton.addEventListener('click', () => {
+        currentIndex++;
+        if (currentIndex >= slides.length) {
+            currentIndex = 0;
+        }
+        updateSlidePosition();
+    });
+
+    prevButton.addEventListener('click', () => {
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slides.length -1;
+        }
+        updateSlidePosition();
+    });
+
+    window.addEventListener('resize', updateSlidePosition);
+});
