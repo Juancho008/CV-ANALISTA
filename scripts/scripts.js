09308749c2054df5d10b7490f83fe980b803a61a
Juancho08 <juanignacioespindola08@gmail.com>
2025-07-14 15:50:08 -0300

    Agregamos una libreria de confeti

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -188,3 +188,48 @@ function enviar(e) {
 
 
 
+let confettiFired = false;
+
+window.addEventListener("scroll", () => {
+  // Altura total de la página
+  const scrollHeight = document.documentElement.scrollHeight;
+  // Cuánto se ha scrolleado + alto ventana
+  const scrollTopPlusWindow = window.scrollY + window.innerHeight;
+
+  // Si estamos a 10px o menos del final y no se disparó aún
+  if (scrollTopPlusWindow >= scrollHeight - 10 && !confettiFired) {
+    confettiFired = true; // para que no se dispare muchas veces
+
+    // Disparo de confetis
+    lanzarConfetti();
+  }
+});
+
+function lanzarConfetti() {
+  const duration = 5 * 1000; // 5 segundos
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 9999 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    // Lanzar confetis de ambos lados de la pantalla
+    confetti(Object.assign({}, defaults, { 
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+    }));
+    confetti(Object.assign({}, defaults, { 
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+    }));
+  }, 250);
+}
